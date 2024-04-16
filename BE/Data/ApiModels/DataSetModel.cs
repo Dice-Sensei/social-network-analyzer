daf7c08787613c0db33b52c347c7bd45c6c6602b
Erik Macháček <dice.sensei@gmail.com>
2024-04-16 17:38:08 +0200

    Add Data project With simple DataSetModel

---
+++ BE/Data/ApiModels/DataSetModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Data.ApiModels
+{
+    public class DataSetModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class DataSetUploadModel : DataSetModel
+    {
+        private string Content { get; set; }
+    }
+}
