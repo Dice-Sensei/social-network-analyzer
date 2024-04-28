4bed82df10c752c9f7fb408e49191b89c470a4e7
Erik Macháček <dice.sensei@gmail.com>
2024-04-28 15:25:11 +0200

    Add override for ToString to DataSet

--- BE/Data/ApiModels/DataSetModel.cs
+++ BE/Data/ApiModels/DataSetModel.cs
@@ -4,10 +4,20 @@
     {
         public long Id { get; set; }
         public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(DataSetModel)} {{{nameof(Id)}: {Id}, {nameof(Name)}: {Name}}}";
+        }
     }
 
     public class DataSetUploadModel : DataSetModel
     {
-        private string Content { get; set; }
+        public string Content { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(DataSetUploadModel)} {{{nameof(Id)}: {Id}, {nameof(Name)}: {Name}, {nameof(Content)}: {Content}}}";
+        }
     }
 }
